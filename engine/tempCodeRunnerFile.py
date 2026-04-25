886342b0c832030a8e24d2ede489bc713d9e43e3
mayank679 <mayankraj6799@gmail.com>
2026-04-25 12:06:16 +0530

    Cleanup: removed temp files and finalized project structure

--- engine/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyB_TPBpzulTV1Pi608iyiztuMyeAxYFD-w")
-
-print("Fetching available models for this API key...\n")
-for m in genai.list_models():
-    print(m.name, "=>", m.supported_generation_methods)
